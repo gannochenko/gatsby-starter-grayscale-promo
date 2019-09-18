5981a809e65c7670bd9b5aa007992c280e6f476f
Sergei <gannochenko.sv@gmail.com>
2019-09-18 17:39:30 +0200

    hmm

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,35 +1,35 @@
 module.exports = {
     pathPrefix: '/blog',
     siteMetadata: {
-        title: `Gatsby Starter `,
-        description: `Gatsby starter `,
-        author: `@awesome1888`,
+        title: 'Gatsby Starter ',
+        description: 'Gatsby starter ',
+        author: '@awesome1888',
     },
     plugins: [
         // 'gatsby-transformer-remark',
-        `gatsby-plugin-react-helmet`,
+        'gatsby-plugin-react-helmet',
         {
-            resolve: `gatsby-source-filesystem`,
+            resolve: 'gatsby-source-filesystem',
             options: {
-                name: `images`,
+                name: 'images',
                 path: `${__dirname}/src/images`,
             },
         },
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
+        'gatsby-transformer-sharp',
+        'gatsby-plugin-sharp',
         {
-            resolve: `gatsby-plugin-manifest`,
+            resolve: 'gatsby-plugin-manifest',
             options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
-                start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
-                display: `minimal-ui`,
-                // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+                name: 'gatsby-starter-default',
+                short_name: 'starter',
+                start_url: '/',
+                background_color: '#663399',
+                theme_color: '#663399',
+                display: 'minimal-ui',
+                // icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
             },
         },
-        `gatsby-plugin-styled-components`,
+        'gatsby-plugin-styled-components',
         'gatsby-plugin-typescript',
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.app/offline
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { Layout } from '../components/Layout';
 import { SEO } from '../components/SEO';
 
+const hz = (one, two) => {};
+
 const HomePage = () => {
     return (
         <Layout>
