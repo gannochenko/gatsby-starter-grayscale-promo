4fb8b3040cf8c8c56afc06adf3fbf2732937e62b
Sergei <gannochenko.sv@gmail.com>
2019-09-18 23:30:10 +0200

    garbage removed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,6 @@ import React from 'react';
 import { Layout } from '../components/Layout';
 import { SEO } from '../components/SEO';
 
-const hz = (one, two) => {};
-
 const HomePage = () => {
     return (
         <Layout>
