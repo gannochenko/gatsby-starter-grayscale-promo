cd3b1ee54bb2d96483115df16cca7af384f05c4c
Sergei <gannochenko.sv@gmail.com>
2019-09-28 21:17:29 +0200

    wip

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
             resolve: `gatsby-source-filesystem`,
             options: {
                 name: `markdown-pages`,
-                path: `${__dirname}/blog`,
+                path: `${__dirname}/content/blocks`,
             },
         },
         'gatsby-transformer-sharp',
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import { graphql } from 'gatsby';
 
 import { Layout } from '../components/Layout';
@@ -6,27 +6,41 @@ import { SEO } from '../components/SEO';
 import { Intro } from '../components/Intro';
 import { TextBlock } from '../components/TextBlock';
 
-// @ts-ignore
-const HomePage = ({ data }) => {
-    console.log(data);
+export interface Node {
+    id: string;
+    html: string;
+}
+
+export interface Data {
+    allMarkdownRemark: {
+        nodes: Node[];
+    };
+}
+
+export interface Props {
+    data: Data;
+}
+
+const HomePage: FunctionComponent<Props> = ({ data }) => {
+    const { allMarkdownRemark: { nodes = [] } = {} } = data;
+
     return (
         <Layout>
             <SEO title="Welcome!" keywords={['']} />
             <Intro />
-            <TextBlock>
-                Амбар в поселке Подлипово Правдинского района Калининградской
-                области радовал глаз туристов и водителей проезжающих авто с XIX
-                века и до 2018 года.
-            </TextBlock>
+            {nodes.map(node => (
+                <TextBlock key={node.id} dangerouslySetInnerHTML={node.html} />
+            ))}
         </Layout>
     );
 };
 
 export const query = graphql`
     query HomePageQuery {
-        site {
-            siteMetadata {
-                description
+        allMarkdownRemark(sort: { fields: frontmatter___sort, order: ASC }) {
+            nodes {
+                id
+                html
             }
         }
     }
